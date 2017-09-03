022ba8ee7cfcadc7ffb9bde6d44f117513f84d2a
Fahad Qazi <fahadqazi@gmail.com>
2017-09-03 23:16:49 +0100

    error fix

--- client/components/App.js
+++ client/components/App.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import * as actionCreators from '../actions/actionCreators';
 import Main from './Main';
 
-function mapDispatchToProps(state){
+function mapStateToProps(state){
   return {
     posts: state.posts,
     comments: state.comments

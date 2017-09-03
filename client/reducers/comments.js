3114ffcb8bcd1161726bcda8c3a0a255e129c87c
Fahad Qazi <fahadqazi@gmail.com>
2017-09-03 22:35:48 +0100

    adding reducers

---
+++ client/reducers/comments.js
@@ -0,0 +1,10 @@
+//a reducer take in two things
+//1. action
+//2. copy of current state
+
+function comments(state=[], action){
+  console.log(state, action);
+  return state;
+}
+
+export default comments;
---
+++ client/reducers/posts.js
@@ -0,0 +1,10 @@
+//a reducer take in two things
+//1. action
+//2. copy of current state
+
+function posts(state=[], action){
+  console.log(state, action);
+  return state;
+}
+
+export default posts;

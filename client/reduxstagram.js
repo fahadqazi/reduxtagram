6592bb7250b87f16202d3d71481879f15eabb484
Fahad Qazi <fahadqazi@gmail.com>
2017-09-03 23:14:20 +0100

    adding provider with app component

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import css from './styles/style.styl';
-import Main from './components/Main';
+import App from './components/App';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 import { Router, Route, IndexRoute, browserHistory} from 'react-router';
 import { Provider } from 'react-redux';
 import store, {history} from './store';
+
 
 const router = (
   <Provider store={store}>

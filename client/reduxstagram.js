fb5bc76f2c0a2991a5a67d5b6b68523f6751e19f
Fahad Qazi <fahadqazi@gmail.com>
2017-09-03 02:19:06 +0100

    setting up routing

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -2,7 +2,19 @@ import React from 'react';
 import {render} from 'react-dom';
 import css from './styles/style.styl';
 import Main from '../components/Main';
+import Single from '../components/Single';
+import PhotoGrid from '../components/PhotoGrid';
+import { Router, Route, IndexRoute, browserHistory} from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:id" component={Single}></Route>
+    </Route>
+  </Router>
+);
 
 render(
-  <Main/>, document.getElementById('root')
+  router, document.getElementById('root')
 )
---
+++ components/Single.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router';
+
+class Single extends Component{
+  render() {
+    return (
+      <div>
+        single page
+      </div>
+    )
+  }
+};
+
+export default Single;

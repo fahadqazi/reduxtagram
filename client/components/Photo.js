47856a8bfbe1a13a00fcf616cabc188480d4cff2
Fahad Qazi <fahadqazi@gmail.com>
2017-09-05 22:54:41 +0100

    adding photo component

---
+++ client/components/Photo.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Photo extends Comment{
+  render() {
+    return(
+      <figure className='grid-figure'>
+      
+      </figure>
+    );
+  }
+}
+
+export default Photo;
